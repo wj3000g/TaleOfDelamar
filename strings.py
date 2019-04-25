c8fd94c55c17e06e282203eb633ad7cb69952dc3
Sandaidev <erwan.egasse@gmail.com>
2019-04-25 21:11:27 +0200

    Rename worldrooms to world
    world will now contain items and rooms

--- strings.py
+++ strings.py
@@ -4,6 +4,7 @@ META_GAMEVERSION = "0.1.0"
 META_GAMECREATOR = "Sandai"
 META_GAMETITLE = META_GAMENAME + " | version " + META_GAMEVERSION
 
+_PLACEHOLDER = "__DEBUG_PLACEHOLDER"
 
 PLAYER_DIALOG_1 = [
     "[Vous] - ugh... j'suis où ? y'a pas de lumière, juste un bouton qui est allumé en vert...",
@@ -20,4 +21,8 @@ PLAYER_DIALOG_1 = [
 PLAYER_DIALOG_BASE1 = [
     "(vous passez par ce qui s'apparente à un SAS détruit...)",
     "(des fissures sont présentes sur les vitres des couloirs de la base abandonnée.)",
+]
+
+PLAYER_DIALOG_DUNES1 = [
+    _PLACEHOLDER
 ]
---
+++ world.py
@@ -0,0 +1,62 @@
+import strings
+
+# Modèle de création de zones :
+
+# "NOM_DE_ZONE": {
+# DESC: Description de la zone,
+# DIALOGENTRY: Entrée dialogue si look() (strings.DIALOG_ENTRY),
+# NEEDITEM: Item requis pour accéder à la zone ,
+# HASITEM: Item disponible dans la zone (affiché si look()),
+# NORTH/SOUTH/EAST/WEST: Nom des zones correspondantes à ces directions,
+# }, ...
+
+
+WORLD_ROOMS = {
+    "CRASHSITE": {
+        "NAME": "Site du crash",
+        "DIALOGENTRY": strings.PLAYER_DIALOG_1,
+        "NEEDITEM": None,
+        "HASITEM": None,
+        "NORTH": "BASE1",
+        "SOUTH": None,
+        "EAST": "DUNES1",
+        "WEST": "DUNES2"
+    },
+
+    "BASE1": {
+        "NAME": "Base abandonnée",
+        "DIALOGENTRY": strings.PLAYER_DIALOG_BASE1,
+        "NEEDITEM": None,
+        "HASITEM": "CROWBAR",
+        "NORTH": "DUNES3",
+        "SOUTH": "CRASHSITE",
+        "EAST": "DUNES1",
+        "WEST": "DUNES2"
+    },
+
+    "DUNES1": {
+        "NAME": ""
+        "DIALOGENTRY": strings.PLAYER_DIALOG_DUNES1,
+        "NEEDITEM": None,
+        "HASITEM": None,
+        "NORTH": "BASE1",
+        "SOUTH": None,
+        "EAST": "MINING2",
+        "WEST": "CRASHSITE",
+    },
+
+    "MINING2" : {
+        "DIALOGENTRY": strings.PLAYER_DIALOG_MINING2,
+        "NEEDITEM": None,
+        "HASITEM": None,
+        "NORTH": "BLACKBOX1",
+        "SOUTH": None,
+        "EAST": None,
+        "WEST": "DUNES1"
+    },
+
+    "BLACKBOX1": {
+        "DIALOGENTRY":
+    }
+
+}
